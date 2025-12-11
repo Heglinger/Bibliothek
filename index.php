56e0ed8980c6045c774c829dd285eb63b8e33a7f
Sasmann38 <eric.hoeglinger@tfs-haslach.at>
2025-12-11 14:48:28 +0100

    8
    ( commit yay

--- index.php
+++ index.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Startseite</title>
+    <style>
+        #suchergebnis td, th {
+            border: 1px solid black;
+        }
+        #suchergebnis {
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     <h1>Schulbibliothek</h1>
+    <h3>Bücher suchen🔎</h3>
+    <form action="index.php" method="post">
+        <input type="text" name="suchbegriff" placeholder="Buch eingeben">
+        <input type="submit" value="Suchen" name="suchen">
+    </form>
+    <?php 
+    if(isset($_POST["suchen"])){
+    $suchbegriff = $_POST["suchbegriff"];
+    $verbindung = mysqli_connect("localhost","root","","Buecherei")
+    or die(mysqli_connect_error());
+    $sql = "SELECT * FROM t_buecher WHERE titel LIKE '$suchbegriff%';";
+    $result = mysqli_query($verbindung,$sql);
+        echo "<table id='suchergebnis'>";
+        echo "<th>Buchnr</th>";
+        echo "<th>Titel</th>";
+        echo "<th>Beschreibung</th>";
+        echo "<th>Autor</th>";
+    while($row = mysqli_fetch_array($result)){
+        echo "<tr><td>".$row['buchnr']."</td>";
+        echo "<td>".$row['titel']."</td>";
+        echo "<td>".$row['beschreibung']."</td>";
+        echo "<td>".$row['autor']."</td></tr><br>";   
+    }
+    echo "</table>"; 
+    }
+    ?>
     <h3>Für Bibliothekare</h3>
     <a href="anmeldung.php">Zum Anmelden</a>
 </body>
